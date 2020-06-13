de78a37f6557fbd4e68d961e6d1a0494608a51b1
Bryan Brkic <bryan@MacBook-Pro.local>
2020-06-13 17:44:23 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Blog Lyfe is a blog application implemented using Django and React. The styling for the application is done using Bootstrap 4.
---
+++ frontend/src/components/BlogDetail.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const BlogDetail = (props) => {
+    const [blog, setBlog] = useState({});
+
+    useEffect(() => {
+        const slug = props.match.params.id;
+
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/${slug}`);
+                setBlog(res.data);
+            }
+            catch (err) {
+
+            }
+        }
+
+        fetchData();
+    }, [props.match.params.id]);
+
+    const capitalizeFirstLetter = (word) => {
+        if (word)
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        else
+            return '';
+    };
+
+    const createBlog = () => {
+        return {__html: blog.content};
+    };
+
+    return (
+        <div className='container mt-3'>
+            <h1 className='display-2'>{blog.title}</h1>
+            <h2 className='text-muted mt-3'>Category: {capitalizeFirstLetter(blog.category)}</h2>
+            <h4 className=''>{blog.month} {blog.day}</h4>
+            <div className='mt-5 mb-5' dangerouslySetInnerHTML={createBlog()} />
+            <hr />
+            <p className="lead mb-5"><Link to='/blog' className="font-weight-bold">Back to Blogs</Link></p>
+        </div>
+    );
+}
+
+export default BlogDetail;
